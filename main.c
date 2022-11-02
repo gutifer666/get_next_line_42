67532816eed47294f91fd20016596bceed28a662
Francisco Javier Gutierrez Perez <frgutier@c3r8s5.42malaga.com>
2022-11-02 11:15:55 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int fd;
+	char *line;
+	fd = open("tests/test1", O_RDONLY);
+	while (1)                           
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		printf("%s", line);
+		free(line);
+	}
+	return (0);
+}
